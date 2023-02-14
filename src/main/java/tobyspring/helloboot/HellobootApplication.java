36742e0b759c9499ddd33219ec699306aa33a744
Gaphun Kim <webjjel@gmail.com>
2023-02-15 00:00:22 +0900

    SpringBootApplication

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,12 +1,11 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -23,6 +22,6 @@ public class HellobootApplication {
 	}
 
 	public static void main(String[] args) {
-		MySpringApplication.run(HellobootApplication.class, args);
+		SpringApplication.run(HellobootApplication.class, args);
 	}
 }
