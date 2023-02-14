eb3dd1d5b847701696ec0e11af8ba106952a8e6f
Gaphun Kim <webjjel@gmail.com>
2023-02-14 23:32:25 +0900

    @Component 스캔

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,12 +1,12 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RequestMapping
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
